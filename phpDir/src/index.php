73a1e480f72085a6225fc5d72ca5e5ceaed66c7e
Pavel-Kliukin <9213087@mail.ru>
2023-05-11 21:57:28 +0300

    Comments changed

--- phpDir/src/index.php
+++ phpDir/src/index.php
@@ -9,7 +9,7 @@
       die("connection failed" . $connection->connect_error);
   }
 
-  // Getting max ID from table "Students"
+  // Gets max ID from table "Students"
   function maxID() {
     global $connection;
 
@@ -21,34 +21,34 @@
     return $maxId;
   }
 
-  // ADD STUDENT
+  // ADDS STUDENT
   if (isset($_POST["addStudent"])) {
     $id = maxId() + 1; // new student's id
-    // Inserting student to DB an Student list
+    // Inserts student to DB an Student list
     $addedName = $_POST["name"];
     $insert_query = "INSERT INTO `Students` (id, name) VALUES('$id', '$addedName')";
     $insert_result = mysqli_query($connection, $insert_query);
-    // to clear $_POST variable and input data:
+    // Clears $_POST variable and input data (it prevents new adding when page refreshed):
     unset($_POST);
     echo '<meta http-equiv="refresh" content="0">';
   }
 
-  // DELETE STUDENT
+  // DELETES STUDENT
   if (isset($_POST["deleteStudent"])) {
 
-    // Inserting student to DB an Student list
+    // Inserts student to DB and Student list
     $deletingID = $_POST["id"];
     $maxID = maxID();
     $delete_query = "DELETE FROM Students WHERE id='$deletingID'";
     $delete_result = mysqli_query($connection, $delete_query);
 
-    // Changing all student's id after id of deleted student
+    // Changes all student's ids that goes after deleted student's id
     for ($id = $deletingID + 1; $id <= $maxID; $id++) {
       $update_query = "UPDATE Students SET id=id-1 WHERE id='$id'";
       $update_result = mysqli_query($connection, $update_query);
     }
 
-    // to clear $_POST variable and input data:
+    // Clears $_POST variable and input data (it prevents new deleting when page refreshed):
     unset($_POST);
     echo '<meta http-equiv="refresh" content="0">';
   }
@@ -68,7 +68,7 @@
 <main>
     <div class="studentsList">
       <?php
-      // Printing all students from DB
+      // Prints all students from DB
       $select_query = "SELECT * FROM Students";
       $select_result = mysqli_query($connection, $select_query);
     
@@ -100,7 +100,7 @@
       </div>
       <div class="teamsOutput">
         <?php
-           //DIVIDE STUDENTS BY TEAMS
+           //DIVIDES STUDENTS BY TEAMS
           if (isset($_POST["divideStudents"])) {
             $team_size = $_POST['howManyStudents'];
             $number_of_students = maxID();
@@ -108,46 +108,39 @@
               echo '<h1>Dividing doesn\'t make sense</h1>';
             } else {
 
-              // to clear $_POST variable and input data:
-              unset($_POST);
-              // echo '<meta http-equiv="refresh" content="0">';
-
               $students_array = range(1, $number_of_students); // [1, 2, 3, ... , maxID]
               $team_number = 1;
               while (count($students_array) > $team_size + 1) {
                 echo "<div class='team'><h2>Team $team_number</h2><div class='names'>";
                   for ($i = 1; $i <= $team_size; $i++){
                     $random_number = rand(0, count($students_array) - 1);
-                    $random_students_id = $students_array[$random_number]; //choose random student's id
+                    $random_students_id = $students_array[$random_number]; //chooses random student's id
                     
-                    //take the name of that random student from DB
+                    //Takes the name of that random student from DB
                     $query = "SELECT * FROM Students WHERE id='$random_students_id'";
                     $query_result = mysqli_query($connection, $query);
                     $random_student = mysqli_fetch_row($query_result)[1];
                     
-                    echo "<p>$i. $random_student</p>"; //print that random student in current team
-                    unset($students_array[$random_number]); //delete that random student from array
+                    echo "<p>$i. $random_student</p>"; //prints that random student in current team
+                    unset($students_array[$random_number]); //deletes that random student from array
                     $students_array = \array_values($students_array); //resets the indexes (keys) of array after deleting element (stupid PHP!)
                   }
                 echo "</div></div>";
                 $team_number++;
               }
-              //output of the last Team (if exist)
+              //Outputs the last Team (if exist)
               $i=1;
               echo "<div class='team'><h2>Team $team_number</h2><div class='names'>";
                 foreach($students_array as $student) {
-                  //take the name of the student from DB
+                  //takes the name of the student from DB
                   $query = "SELECT * FROM Students WHERE id='$student'";
                   $query_result = mysqli_query($connection, $query);
                   $random_student = mysqli_fetch_row($query_result)[1];
                   
-                  echo "<p>$i. $random_student</p>"; //print student in current team
+                  echo "<p>$i. $random_student</p>"; //prints student in current team
                   $i++;
                 }
               echo "</div></div>";
-              
-              // unset($_POST);
-              // echo '<meta http-equiv="refresh" content="0">';
             }
           }
         ?>
