b57ee5279f01a99146c189fa09ffa25109893562
Pavel-Kliukin <9213087@mail.ru>
2023-05-12 22:03:54 +0300

    Style done

--- phpDir/src/index.php
+++ phpDir/src/index.php
@@ -26,6 +26,7 @@
     $id = maxId() + 1; // new student's id
     // Inserts student to DB an Student list
     $addedName = $_POST["name"];
+    $addedName = str_replace("'","''",$addedName); //sanitizing from " ' "
     $insert_query = "INSERT INTO `Students` (id, name) VALUES('$id', '$addedName')";
     $insert_result = mysqli_query($connection, $insert_query);
     // Clears $_POST variable and input data (it prevents new adding when page refreshed):
@@ -60,58 +61,68 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="index.css">
   <title>Random teams</title>
 </head>
 <body>
-<h1>Students list</h1>
-<main>
-    <div class="studentsList">
-      <?php
-      // Prints all students from DB
-      $select_query = "SELECT * FROM Students";
-      $select_result = mysqli_query($connection, $select_query);
-    
-      while ($row = mysqli_fetch_assoc($select_result)) {
-        foreach ($row as $value) {
-          echo ($value) . ". ";
+  <div>
+    <header><h1>STUDENTS |&#8592; &nbsp;&nbsp; &rarr;| DIVIDER</h1></header>
+    <main>
+      <div class="studentsList">
+        <h2>Students list</h2>
+        <?php
+        // Prints all students from DB
+        $select_query = "SELECT * FROM Students";
+        $select_result = mysqli_query($connection, $select_query);
+      
+        while ($row = mysqli_fetch_assoc($select_result)) {
+          foreach ($row as $value) {
+            echo ($value) . ". ";
+          }
+          echo "<br>";
         }
-        echo "<br>";
-      }
-      ?>
-    </div>
-    <div class="rightSide">
+        ?>
+      </div>
       <div class="forms">
         <form action="index.php" id="addForm" method="POST">
           <lable>Add student</lable>
-          <input type="text" name="name" placeholder="John Doe" required/>
-          <button type='submit' name="addStudent">Submit</button>
+          <div>
+            <input type="text" name="name" placeholder="John Doe" required/>
+            <button type='submit' name="addStudent">Submit</button>
+          </div>
         </form>
         <form action="index.php" id="deleteForm" method="POST">
-          <lable>Delete by id</lable>
-          <input type="number" name="id" placeholder="12" required/>
-          <button type='submit' name="deleteStudent">Delete</button>
+          <lable>Delete student by id</lable>
+          <div>
+            <input type="number" name="id" placeholder="12" required/>
+            <button type='submit' name="deleteStudent">Delete</button>
+          </div>
         </form>
         <form action="index.php" id="howManyForm" method="POST">
           <lable>Students per team</lable>
-          <input type="number" name="howManyStudents" required/>
-          <button type='submit' name="divideStudents">Divide</button>
+          <div>
+            <input type="number" name="howManyStudents" required/>
+            <button type='submit' name="divideStudents">Divide</button>
+          </div>
         </form>
       </div>
       <div class="teamsOutput">
         <?php
-           //DIVIDES STUDENTS BY TEAMS
+            //DIVIDES STUDENTS BY TEAMS
           if (isset($_POST["divideStudents"])) {
             $team_size = $_POST['howManyStudents'];
             $number_of_students = maxID();
             if ($team_size < 2 || $number_of_students - $team_size < 2) {
               echo '<h1>Dividing doesn\'t make sense</h1>';
             } else {
-
+  
               $students_array = range(1, $number_of_students); // [1, 2, 3, ... , maxID]
               $team_number = 1;
               while (count($students_array) > $team_size + 1) {
-                echo "<div class='team'><h2>Team $team_number</h2><div class='names'>";
+                echo "<div class='team' style='display:none;'><h2>Team $team_number</h2><div class='names'>";
                   for ($i = 1; $i <= $team_size; $i++){
                     $random_number = rand(0, count($students_array) - 1);
                     $random_students_id = $students_array[$random_number]; //chooses random student's id
@@ -130,7 +141,7 @@
               }
               //Outputs the last Team (if exist)
               $i=1;
-              echo "<div class='team'><h2>Team $team_number</h2><div class='names'>";
+              echo "<div class='team' style='display:none;'><h2>Team $team_number</h2><div class='names'>";
                 foreach($students_array as $student) {
                   //takes the name of the student from DB
                   $query = "SELECT * FROM Students WHERE id='$student'";
@@ -144,9 +155,26 @@
             }
           }
         ?>
-      </div>
-    </div>
-</main>
+        <script>
+          // This script shows teams one by one with a delay of 1 second
+          const teams = document.querySelectorAll('.team');
+          let index = 0;
 
+          function showNextTeam() {
+            if (index < teams.length) {
+              teams[index].style.display = 'block';
+              teams[index].style.animation = 'blur 2s ease';
+              teams[index].style.animationDelay = '-1.5s';
+              index++;
+              setTimeout(showNextTeam, 500); // 0.5 second delay
+            }
+          }
+
+          showNextTeam();
+        </script>
+      </div>
+    </main>
+  </div>
+<footer>Pavel Kliukin 2023 &copy;</footer>
 </body>
 </html>
